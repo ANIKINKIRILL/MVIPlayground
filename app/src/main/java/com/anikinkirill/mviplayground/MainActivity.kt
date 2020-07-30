80bf34ae731c324e286e71c30ad01de561759514
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-30 16:18:35 +0300

    MVI starting point

--- app/src/main/java/com/anikinkirill/mviplayground/MainActivity.kt
+++ app/src/main/java/com/anikinkirill/mviplayground/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showMainFragment()
     }
+
+    private fun showMainFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, MainFragment(), "MainFragment")
+            .commit()
+    }
+
 }
