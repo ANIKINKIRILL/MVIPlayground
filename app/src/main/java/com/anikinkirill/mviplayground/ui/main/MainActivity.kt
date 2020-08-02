d7b2a00f98c6cd99d567f00112aab8a25f62c5c6
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-02 10:38:12 +0300

    Finish DataStateListeners logic
    Apply DataStateListeners to a MainActivity

--- app/src/main/java/com/anikinkirill/mviplayground/ui/main/MainActivity.kt
+++ app/src/main/java/com/anikinkirill/mviplayground/ui/main/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.anikinkirill.mviplayground.ui.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.anikinkirill.mviplayground.R
+import com.anikinkirill.mviplayground.ui.DataStateListener
+import com.anikinkirill.mviplayground.util.DataState
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DataStateListener {
 
     private lateinit var viewModel: MainViewModel
 
@@ -25,6 +30,23 @@ class MainActivity : AppCompatActivity() {
                 R.id.fragment_container,
                 MainFragment(), "MainFragment")
             .commit()
+    }
+
+    override fun onDataStateChange(dataState: DataState<*>?) {
+
+        dataState?.let {
+            // LOADING
+            if(it.loading) {
+                progress_bar.visibility = View.VISIBLE
+            }else{
+                progress_bar.visibility = View.GONE
+            }
+
+            // ERROR
+            it.message?.let { errorMessage ->
+                Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
